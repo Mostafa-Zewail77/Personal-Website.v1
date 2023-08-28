4f3e438d1497bb20746e91a7f60141d6ea9df7b0
Mostafa Zewail77 <mostafazewail77@gmail.com>
2023-08-28 21:30:32 +0300

    Edit nav

--- src/components/myNav/MyNav.jsx
+++ src/components/myNav/MyNav.jsx
@@ -9,13 +9,13 @@ function MyNav() {
         <Navbar bg="transparent" className='d-none d-lg-block' >
          
             <Nav className="mt-4 flex-column">
-              <Nav.Link as={Link} to="about" smooth={true} duration={50} spy={true} offset={-550} activeClass="active">
+              <Nav.Link as={Link} to="about" smooth={true} duration={50} spy={true} offset={-560} activeClass="active">
                 About
               </Nav.Link> 
-              <Nav.Link as={Link} to="projects" smooth={true} duration={50} spy={true} offset={-550} activeClass="active">
+              <Nav.Link as={Link} to="projects" smooth={true} duration={50} spy={true} offset={-560} activeClass="active">
                 Projects
               </Nav.Link> 
-              <Nav.Link as={Link} to="contact" smooth={true} duration={50} spy={true} offset={-550} activeClass="active">
+              <Nav.Link as={Link} to="contact" smooth={true} duration={50} spy={true} offset={-560} activeClass="active">
                 Contact
               </Nav.Link>     
             </Nav>
