1f4c147b1b523e3e5c7b1d848ee703c24c769744
Mostafa Zewail77 <mostafazewail77@gmail.com>
2023-09-20 18:19:12 +0300

    Edit Nav

--- src/components/myNav/MyNav.jsx
+++ src/components/myNav/MyNav.jsx
@@ -4,21 +4,49 @@ import { Nav, Navbar } from 'react-bootstrap';
 import { Link } from 'react-scroll';
 
 function MyNav() {
-
+  
     return (
-        <Navbar bg="transparent" className='d-none d-lg-block' >
+        <Navbar bg="transparent" className='d-none d-lg-block'  >
          
-            <Nav className="mt-4 flex-column">
-              <Nav.Link as={Link} to="about" smooth={true} duration={50} spy={true} offset={-560} activeClass="active">
-                About
-              </Nav.Link> 
-              <Nav.Link as={Link} to="projects" smooth={true} duration={50} spy={true} offset={-560} activeClass="active">
-                Projects
-              </Nav.Link> 
-              <Nav.Link as={Link} to="contact" smooth={true} duration={50} spy={true} offset={-560} activeClass="active">
-                Contact
-              </Nav.Link>     
-            </Nav>
+          <Nav className="mt-4 flex-column">
+            <Link
+              activeClass="active"
+              className="nav-link"
+              to="about"
+              // href='#about'
+              spy={true}
+              smooth={true}
+              duration={50}
+              offset={-565}
+            >
+              About
+            </Link>
+            
+            <Link
+              activeClass="active"
+              className="nav-link"
+              to="projects"
+              // href='#projects'
+              spy={true}
+              smooth={true}
+              duration={50}
+              offset={-563}
+            >
+              Projects
+            </Link>
+            <Link
+              activeClass="active"
+              className="nav-link"
+              to="contact"
+              // href='#contact'
+              spy={true}
+              smooth={true}
+              duration={50}
+              offset={-560}
+            >
+              Contact
+            </Link> 
+          </Nav>
          
         </Navbar>
       );
