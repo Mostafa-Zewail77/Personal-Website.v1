b6fa63e7a6e0b35d469f689c79fceef32a65340d
Mostafa Hassan <mostafazewail77@gmail.com>
2024-08-29 20:26:07 +0300

    Update about section

--- src/containers/about/About.jsx
+++ src/containers/about/About.jsx
@@ -9,18 +9,17 @@ function About() {
         <Heading sectionHeader= 'About Me'/>
 
         <p>
-          Hi, I'm Mostafa, and I've been coding for a while now, mainly focusing on creating web applications.<br/>
-          I'm a self-taught software developer passionate about creating things that live on the Internet.  
-          I started from scratch and fell in love with programming along the way. 
+          Hi there! I'm Mostafa, a self-taught software developer with a passion for bringing ideas to life on the Internet.
+          I started my coding journey from scratch and fell in love with programming along the way. 
           There's something incredibly satisfying about turning ideas into real, working applications.
         </p>
         <p>
-          My expertise lies in front-end development, particularly in React JS. 
-          I've built a range of projects and continually learn to stay ahead in the field.
+          My expertise lies in front-end development, especially with React JS. 
+          I've built a variety of projects and I'm always eager to learn and grow.
         </p>
         <p>
-          I'm all about collaboration. Whether it's with clients or teams, I enjoy working together to deliver solutions that drive results.
-          I'm always on the lookout for new opportunities to grow my skill set and contribute to successful projects.
+          I believe in the power of collaboration. Whether working with clients or teaming up with colleagues, I enjoy the process of working together to create solutions that make a difference.
+          I'm always looking for new opportunities to grow my skill set and contribute to successful projects.
         </p>
         <p> Here are some technologies I’ve been working with:</p>
         <Row className=' techs pe-md-5'>
