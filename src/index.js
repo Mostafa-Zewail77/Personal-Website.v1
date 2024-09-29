b87448155a7ead363b0d6d0cb1a21f4ee2e598bf
Mostafa Hassan <mostafazewail77@gmail.com>
2024-09-29 11:56:08 +0300

    Integrate Vercel SpeedInsights

--- src/index.js
+++ src/index.js
@@ -5,12 +5,14 @@ import App from './App';
 import './index.css';
 import { BrowserRouter} from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <HelmetProvider>
+        <SpeedInsights />
         <App/>
       </HelmetProvider>
       
